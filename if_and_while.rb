d8a2f65b8f99506a42013cb3f3fbc0d543e157da
decobisu <decobisu@miyako-2.local>
2014-03-05 01:04:21 +0900

    一章の最後まで

---
+++ if_and_while.rb
@@ -0,0 +1,19 @@
+a = 20
+puts a
+if a >= 10
+    print "bigger\n"
+end
+if a <= 9
+    print "smaller\n"
+end
+
+i = 1
+while i <= 10
+    print i, "\n"
+    i = i + 1
+end
+
+100.times do
+    print "ふぁぁぁぁぁぁぁ\n"
+end
+
